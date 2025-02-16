a3ef99a91f0bf9176163c5b9ba6709e7d831e60a
1F10220082 <s1f102200828@iniad.org>
2025-02-17 01:38:55 +0900

    CSSの変更

--- main/form.py
+++ main/form.py
@@ -5,13 +5,13 @@ class CreateGoalForm(forms.Form):
     title = forms.CharField(
         label="目標のタイトル",
         widget=forms.TextInput(
-            attrs = {"placeholder": "CSS仕事でを使えるようになる  FPの資格を取る  ..."}
+            attrs = {"placeholder": "例: CSSを仕事でを使えるようになる  FPの資格を取る  ..."}
         )
     )
     description = forms.CharField(
         label="目標の説明・内容",
         widget=forms.Textarea(
-            attrs={"placeholder": "〇〇ができるようになるために△△が必要になる。  FPはフィナンシャルプランナーの略であり、詳細は......  "}
+            attrs={"placeholder": "例: 〇〇ができるようになるために△△が必要になる。  FPはフィナンシャルプランナーの略であり、詳細は......  "}
         )
     )
 
